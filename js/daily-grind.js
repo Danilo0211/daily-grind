7f66c152bb5e4bf04ac5fc6a98f57cf390f193b8
Danilo0211 <166162979+Danilo0211@users.noreply.github.com>
2026-05-19 08:30:02 -0700

    adding Monday

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,3 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let.myDate = new Date();
 
 let.myDay = myDate.getDay();
@@ -8,6 +24,18 @@ switch(myDay){
 
  	case 0:
     		today = "Sunday";
+ 	break;
+
+    case 1:
+   		today = "Monday";
+        coffee = {
+            name:"Bubble Tea:",
+            pic:"bubble-tea.jpg",
+            alt:"A pic of a yummy bubble tea.",
+            color:"pink",
+            day:"Tuesday",
+            desc:`I like me some bubble tea!` 
+        };
  	break;
 
  	case 2:
