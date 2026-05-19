141a48b705ac1fb72b3433b9d9f5ce38b3c085e3
Danilo0211 <166162979+Danilo0211@users.noreply.github.com>
2026-05-18 18:03:26 -0700

    first version of coffee

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,1 +1,35 @@
-alert("hello from daily-grind.js!");
+
+
+
+let.myDate = new Date();
+
+let.myDay = myDate.getDay();
+
+let.today = ""
+
+switch(myDay){
+
+ 	case 0:
+    		today =  "Sunday";
+ 	break;
+
+ 	case 2:
+   		today = "Tuesday";
+        coffee = {
+            name:"Bubble Tea:",
+            pic:"bubble-tea.jpg",
+            alt:"A pic of a yummy bubble tea.",
+            color:"pink",
+            day:"Tuesday",
+            desc:`I like me some bubble tea!` 
+        };
+ 	break;
+
+ 	default:
+    	today = "Something went wrong!";
+
+}
+
+sonsole.log(coffee);
+
+alert("today");
